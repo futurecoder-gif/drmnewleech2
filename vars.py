d9ce8a84637725be818c9e95a970482708ee996e
drbestie <educationmail06@gmail.com>
2025-06-29 07:07:36 +0530

    drbestie

---
+++ vars.py
@@ -0,0 +1,13 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "22849789"))
+API_HASH = environ.get("API_HASH", "0fc127c6055acd59f00ec6c229e1e3c4")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7932500734:AAE8a2qrZfjTTslR3OWSy1z8w1JGHnVZDAk")
+OWNER = int(environ.get("OWNER", "7296271316"))
+CREDIT = "𝓨𝓪𝓼𝓼𝓾 𝓣𝓱𝓪𝓴𝓾𝓻"
+LOG_CHANNEL = "--1002477114210" # log group me bot ko admin narur banaye full rights ke sath
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
